e5739bfceafc5aad50fce09d626ee29894778f6f
Gerry Nelson <gerrynelson63@yahoo.com>
2022-12-28 14:50:23 -0500

    update

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
 from exif import Image
 
+from datetime import datetime
+
+file="2022-05-07-16-29-0023.jpg"
 dir="/home/sasgnn/images/ConthroughHighSchool"
 
-with open('grand_canyon.jpg', 'rb') as image_file:
+with open(dir+'/'+file, 'rb') as image_file:
     my_image = Image(image_file)
-    my_image.has_exif
+    hasexif=my_image.has_exif
+    print(hasexif)
+    exif_list=my_image.list_all()
+    print(exif_list)
+    now = datetime.now()
+    my_image.datetime=now
