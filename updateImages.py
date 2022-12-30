9e6911ece57ef0c5866c7cac2c43b645269e55c1
Gerry Nelson <45608078+gerrynelson63@users.noreply.github.com>
2022-12-29 22:13:16 -0500

    update and rename

---
+++ updateImages.py
@@ -0,0 +1,80 @@
+from exif import Image as ExifImage
+
+from datetime import datetime
+
+import argparse, sys, filedate, os
+
+parser = argparse.ArgumentParser(description="Update exif image metadata.")
+parser.add_argument("-id","--directory", help="Directory that contains Image files to update",default="/home/sasgnn/images/ConthroughHighSchool")
+parser.add_argument("-od","--output", help="Directory to output new files.",default="/home/sasgnn/test")
+parser.add_argument("-dt","--datetime", help="Datetime to set on Image",default="1999:05:06 12:00:00")
+
+parser.add_argument("-q","--quiet", help="Suppress the are you sure prompt.", action='store_true')
+args= parser.parse_args()
+
+basedir=args.directory
+outputdir=args.output
+newdatetime=args.datetime
+
+if os.path.isdir(basedir):
+
+    # loop files in the directory
+    for filename in os.listdir( basedir ):
+        img_file=os.path.join(basedir,filename)
+        with open(img_file, 'rb') as image_file:
+           my_image = ExifImage(image_file)
+
+        hasexif=my_image.has_exif
+        #print(hasexif)
+        exif_list=my_image.list_all()
+        print(exif_list)
+
+        print ("Processing File: "+img_file) 
+        # for tag in exif_list:
+        #     # Get the Value of the Tag in the Image
+        #     value = my_image.get(tag)
+
+        #     # Print the Tag/Value Pair to the Terminal Window
+        #     print("{}: {}".format(tag, value))
+        
+        my_image.datetime_original=newdatetime
+        my_image.datetime=newdatetime
+        my_image.usercomment="Scanned by Connie"
+
+        output_filepath=os.path.join(outputdir,filename)
+
+        with open(output_filepath,'wb') as ofile:
+            ofile.write(my_image.get_file())
+            
+        with open(output_filepath, 'rb') as new_image_file: 
+            new_image = ExifImage(new_image_file)
+        
+        exif_list2=new_image.list_all()
+
+        for tag in exif_list2:
+            # Get the Value of the Tag in the Image
+            value = new_image.get(tag)
+
+            # Print the Tag/Value Pair to the Terminal Window
+            print("{}: {}".format(tag, value))
+        
+        # update create and modify date of new file
+        a = output_filepath
+        a_file = filedate.File(a)
+ 
+        a_file.set(
+            created = newdatetime,
+            modified = newdatetime,
+            accessed = newdatetime
+        )
+
+        after = filedate.File(a)
+        print(after.get())
+
+
+""" 
+for root, dirs, file_names in os.walk(dir):
+        for file_name in file_names:
+            if file_name.lower().endswith(('jpg', 'png')):
+                img_path = os.path.join(root, file_name)
+                fix_image_dates(img_path) """
